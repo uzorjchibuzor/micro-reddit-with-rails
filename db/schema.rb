0906a81cf7b7d2a1a48ccc08863f3ddfab737f1e
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2018-10-18 13:06:41 +0100

    generated Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_105355) do
+ActiveRecord::Schema.define(version: 2018_10_18_120115) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
